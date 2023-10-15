a7c7e5506c097b8dfe20da70adad5ceda801278d
Dan Sohval <daniel.sohval@gmail.com>
2023-10-15 17:53:40 -0400

    adds starter point

--- script.js
+++ script.js
@@ -7,10 +7,61 @@ document.addEventListener("DOMContentLoaded", function() {
         const text = textInput.value.trim();
 
         if (text !== "") {
-            const p = document.createElement("p");
-            p.textContent = text;
-            output.appendChild(p);
+            const div = document.createElement("div");
+            div.innerHTML = text;
+            output.appendChild(div);
             textInput.value = ""; // Clear the textarea
         }
     });
 });
+
+/*
+Turns a single line of text into an array of lower-case strings,
+with special characters removed
+*/
+function  parseText(text){ 
+    return text
+            .toLowerCase()
+            .replace(/[^a-z\s]/ig, "")
+            .split(' ');
+}
+
+// Your code below
+
+/*
+ Creates an "Markov Chain" representation of a song's lyrics.
+ This consists of an object, with a key for each individual lyric in the song.
+ Values for each key is a list of strings for each lyric that follows the key
+ 
+ Example
+ lyrics = 'It was the best of times, it was the worst of times'
+
+ output = {
+  'it' : ['was'],
+  'was' : ['the'],
+  'the' : ['best'] ,
+  'best' : ['of' ],
+  'of' : ['best', 'worst'],
+  'times' : ['it'],
+  'worst' : ['of']
+ }
+*/
+function generateWordpairs(lyrics) {
+    return; 
+}
+
+/*
+ Given the object returned from generateWordPairs,
+ Write a line of generated lyrics, of length len
+*/
+function writeLine(wordpairs, len) {
+    return;
+}
+
+/*
+Given a long string representing lyrics to a song,
+generate a mock verse `len` lines long using individual words in the lyrics
+*/
+function generateLyrics(lyrics, len) {
+    return;
+}
